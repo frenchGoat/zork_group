8ed9f38a409159115b6011f6340fdfa51ef8a005
frenchGoat <jacques.troussard@gmail.com>
2016-11-08 15:34:00 -0500

    **Javadocs mostly done -> decide canned or trigger sensitive return string

--- src/DisappearEvent.java
+++ src/DisappearEvent.java
@@ -7,18 +7,31 @@
  *
  */
 public class DisappearEvent extends Event {
+	
+	/**
+	 * Primary name of target item Object. 
+	 */
 
 	private String itemName;
 	
 	/**
-	 * @param itemName String - primary name of Item which is to be removed from game.
+	 * This is an item manipulation trigger, in which the target item is removed from the
+	 * game completely. This is used in conjuction with transform events as well when
+	 * finite items are depleted or broken.
+	 * 
+	 * @param itemName primary name of Item which is to be removed from game
 	 */
 	public DisappearEvent(String itemName) {
 		this.itemName = itemName;
 	}
 
-	/* (non-Javadoc)
-	 * @see Event#trigger()
+	/**
+	 * Activates the DisappearEvent. 
+	 * +Return statement should be tailored to the situation?
+	 * ++Tiggering via usage or breaking will alert the user?
+	 * ++Transformation triggers remain silent?
+	 * 
+	 * @return +should this be canned or have multiple results?
 	 */
 	@Override
 	String trigger() {
