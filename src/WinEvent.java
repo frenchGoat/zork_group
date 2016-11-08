9812111b330739ba9462d639f011b8d83a2229aa
frenchGoat <jacques.troussard@gmail.com>
2016-11-07 20:23:33 -0500

    add WinEvent skeleton + javadoc

---
+++ src/WinEvent.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+
+/**
+ * @author Jacques Troussard
+ *
+ */
+public class WinEvent extends Event {
+	
+	public WinEvent() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/* (non-Javadoc)
+	 * @see Event#trigger()
+	 */
+	@Override
+	String trigger() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
