6e0864dfb124dd8f8a4dcc49eb953823c9d54b31
frenchGoat <jacques.troussard@gmail.com>
2016-11-08 18:04:59 -0500

    ***javadocs done

--- src/ItemSpecificCommand.java
+++ src/ItemSpecificCommand.java
@@ -5,7 +5,9 @@ class ItemSpecificCommand extends Command {
                         
 
     /**
-     * Command structure takes in parsed user input and  
+     * Command structure takes in parsed user input from CommandFactory and assigns 
+     * instance variables; verb and noun.  
+     * 
      * @param verb
      * @param noun
      */
@@ -14,6 +16,13 @@ class ItemSpecificCommand extends Command {
         this.noun = noun;
     }
 
+    /**
+     * After confirming target Item object(noun) exists within the vicinity of the player, the
+     * message/event bank for that item is accessed by the verb and the appropriate
+     * events are triggered. Return string is printed to the console.
+     * 
+     * @return item/event message to be printed to console
+     */
     public String execute() {
         
         Item itemReferredTo = null;
