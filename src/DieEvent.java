8e6d0506c7bf4964939ae054702ee10ae9de19b3
frenchGoat <jacques.troussard@gmail.com>
2016-11-07 20:26:41 -0500

    add DieEvent class + javadocs

---
+++ src/DieEvent.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+
+/**
+ * @author Jacques Troussard
+ *
+ */
+public class DieEvent extends Event {
+
+	/**
+	 * 
+	 */
+	public DieEvent() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/* (non-Javadoc)
+	 * @see Event#trigger()
+	 */
+	@Override
+	String trigger() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
