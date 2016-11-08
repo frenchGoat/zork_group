f4244bfbbec2493d60bd672f0b57657215ed4527
frenchGoat <jacques.troussard@gmail.com>
2016-11-08 14:27:33 -0500

    **Mostly finished -> decide end game behavior?

--- src/DieEvent.java
+++ src/DieEvent.java
@@ -9,14 +9,21 @@
 public class DieEvent extends Event {
 
 	/**
-	 * 
+	 * This is an end game trigger, in which the player has lost. Die events can be
+	 * caused by reaching 0 in either player health or hunger status. This trigger 
+	 * +should reload the game from last restore point
+	 * +should reload the game from a fresh game
+	 * +should ask the player if they want to play again
+	 * +should just quit the program
 	 */
 	public DieEvent() {
 		// TODO Auto-generated constructor stub
 	}
 
-	/* (non-Javadoc)
-	 * @see Event#trigger()
+	/**
+	 * Activates the DieEvent.
+	 * 
+	 * @return end of game message
 	 */
 	@Override
 	String trigger() {
