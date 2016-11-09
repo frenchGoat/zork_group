1f8358c06c1b4455f2560a933ec8a0a7ad6121a0
frenchGoat <jacques.troussard@gmail.com>
2016-11-08 19:14:54 -0500

    created keys class

---
+++ src/Keys.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+/**
+ * University of Mary Washington
+ * CPSC 240 Section 2
+ * 
+ */
+
+/**
+ * @author 
+ */
+public class Keys extends Item {
+
+	Keys(Scanner s) throws NoItemException, Dungeon.IllegalDungeonFormatException,
+			Item.NoItemException {
+		super(s);
+		// TODO Auto-generated constructor stub
+	}
+
+}
