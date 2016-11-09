b20a1c23337d1045ba44bbea9ce59f6b51270d49
frenchGoat <jacques.troussard@gmail.com>
2016-11-09 18:32:46 -0500

    +++ added custom exception, name field, set basic constructor, created getter for name field, needs lots more work, just made the basics to finish room class

--- src/NPC.java
+++ src/NPC.java
@@ -4,8 +4,27 @@
 
 /**
  * @author Kaylee Payne
+ * @author Jacques Troussard
  *
  */
 public class NPC {
+	
+    @SuppressWarnings("serial")
+	static class NoNPCException extends Exception {}
+    
+    /**
+     * Name of NPC
+     */
+    private String name;
+    
+    NPC(String n) {
+    	this.name = n;
+    }
+
+	public String getName() {
+		return name;
+	}
+    
+    
 
 }
