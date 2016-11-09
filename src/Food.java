df868476209d1b25420a1b92a0f466e14af9f13e
frenchGoat <jacques.troussard@gmail.com>
2016-11-08 19:30:32 -0500

    created food class

---
+++ src/Food.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+/**
+ * University of Mary Washington
+ * CPSC 240 Section 2
+ */
+
+/**
+ * @author Jacques Troussard
+ *
+ */
+public class Food extends Item {
+
+	/**
+	 * @param s
+	 * @throws NoItemException
+	 * @throws Dungeon.IllegalDungeonFormatException
+	 */
+	public Food(Scanner s) throws NoItemException, Dungeon.IllegalDungeonFormatException {
+		super(s);
+		// TODO Auto-generated constructor stub
+	}
+
+}
