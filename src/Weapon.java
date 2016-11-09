524be29958bfe6e4a883204dbfb03cf73d747f52
frenchGoat <jacques.troussard@gmail.com>
2016-11-08 19:32:07 -0500

    created Weapon class skeleton

---
+++ src/Weapon.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+import Dungeon.IllegalDungeonFormatException;
+
+/**
+ * 
+ */
+
+/**
+ * @author Jacques Troussard
+ * @author Kaylee Payne
+ *
+ */
+public class Weapon extends Item {
+
+	/**
+	 * @param s
+	 * @throws NoItemException
+	 * @throws Dungeon.IllegalDungeonFormatException
+	 */
+	public Weapon(Scanner s) throws NoItemException, Dungeon.IllegalDungeonFormatException {
+		super(s);
+		// TODO Auto-generated constructor stub
+	}
+
+}
