92d7e419436ce01080961d89fc29c1afb6db59ca
frenchGoat <jacques.troussard@gmail.com>
2016-11-09 21:44:24 -0500

    *JavaDocs Done.

--- src/UnknownCommand.java
+++ src/UnknownCommand.java
@@ -1,11 +1,28 @@
+/**
+ * @author Jacques Troussard
+ *
+ */
 class UnknownCommand extends Command {
 
+	/**
+	 * User input that wasn't parsed into a predefined command
+	 */
     private String bogusCommand;
 
+    /**
+     * Takes a string argument and assigns it to the instance variable, bogusCommand.
+     * @param bogusCommand
+     */
     UnknownCommand(String bogusCommand) {
         this.bogusCommand = bogusCommand;
     }
 
+    /**
+     * Prints error message alerting the player that their command could not be parsed
+     * into a valid command.
+     * 
+     * @return error message
+     */
     String execute() {
         return "I'm not sure what you mean by \"" + bogusCommand + "\".\n";
     }
