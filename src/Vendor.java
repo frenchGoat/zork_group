3ea0ce14a15f21a9ab8623fc1d1d1a64a4b4ae5f
kpayne4 <kpayne4@mail.umw.edu>
2016-11-09 21:12:02 -0500

    Create Vendor.java

---
+++ src/Vendor.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+/**
+ * The Vendor extends NPC and has an additional field for the currency it carries.
+ * It will be able to trade items with the player for set amounts of currency.
+ * 
+ * @author Kaylee payne
+ */
+public class Vendor extends NPC {
+    /**
+     * Money int that determines the amount of money thr vendor possesses.
+     */
+    private int money;
+    /**
+    * Creates a Vendor NPC with an int money.
+    *
+    * @param money A int value of money owned.
+    */
+    Vendor(int money) {
+        this.money = money;
+    }
+    
+    /**
+    * Creates a Vendor NPC with a Scanner that reads from a dungeon file.
+    *
+    * @param scanner The scanner of the dungeon file.
+    */
+    Vendor(Scanner scanner) {
+        
+    }
+    
+    /**
+     * Returns the amount of money this Vendor has.
+     * 
+     * @return money The int value of money this Vendor has.
+     */
+    public int getMoney() {
+        
+    }
+    
+    /**
+     * Will return the Item purchesed whent he correct amount of money is given.
+     * 
+     * @param price The value of the Item the player is buying.
+     * @return itemPurchased The Item from the Vendor's inventory that was purchssed.
+     */
+    public Item trade(int price) {
+        
+    }
+    
+    /**
+     * Will refund the player the money equal to the value of the Item given.
+     * 
+     * @param offer The Item the player is giving to the Vendor.
+     * @return refund The int amount of money that the offer Item is worth.
+     */    
+    public int trade(Item offer) {
+        
+    }
+}
