295dbdfd939abd44c8122099b15f822fc70ca73e
Jacques Troussard <jtroussa@mail.umw.edu>
2016-11-16 14:49:30 -0500

    changed Item object eventParts spliting mech from '//[' to using the Pattern.quote()

--- src/Item.java
+++ src/Item.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.regex.Pattern;
 import java.util.Hashtable;
 
 /**
@@ -7,119 +8,119 @@ import java.util.Hashtable;
  */
 public class Item {
 
-    @SuppressWarnings("serial")
-	static class NoItemException extends Exception {}
+	@SuppressWarnings("serial")
+	static class NoItemException extends Exception {
+	}
 
-    /**
-     * Primary name of item, used in game as id, used in data structures as key.
-     */
-    private String primaryName;
-    
-    /**
-     * Weight of item.
-     */
-    private int weight;
-    
-    /**
-     * Hashtable used to store messages and events related to item.
-     */
-    private Hashtable<String, String> messages;
-    /**
-     * ArrayList used to store events related to item specific commands. Events
-     * are keyed by their corresponding verb (itemSpecific command). example:
-     * file read in -> drink[Transform(emptyCan), Wound(-1)]:'Generic Message'
-     * would store the Event Transform(emptyCan) keyed to 'drink', and the Event
-     * Wound(-1) keyed to 'drink'. The event factory will be passed this db and
-     * all necessary events keyed to action verb the command factory parsed will
-     * be invoked.
-     */
-    private Hashtable<String, String> triggers;
-    
-    /**
-     * Takes w as a int parameter and assigns it to the instance variable, weight.
-     * 
-     * @param w amount of space modified when taken into inventory.
-     */
-    Item(int w) {
-    	this.weight = w;
-    }
-    
-    Item(String n) {
-    	this.primaryName = n;
-    }
+	/**
+	 * Primary name of item, used in game as id, used in data structures as key.
+	 */
+	private String primaryName;
+	/**
+	 * Weight of item.
+	 */
+	private int weight;
+	/**
+	 * Hashtable used to store messages and events related to item.
+	 */
+	private Hashtable<String, String> messages;
+	/**
+	 * ArrayList used to store events related to item specific commands. Events
+	 * are keyed by their corresponding verb (itemSpecific command). example:
+	 * file read in -> drink[Transform(emptyCan), Wound(-1)]:'Generic Message'
+	 * would store the Event Transform(emptyCan) keyed to 'drink', and the Event
+	 * Wound(-1) keyed to 'drink'. The event factory will be passed this db and
+	 * all necessary events keyed to action verb the command factory parsed will
+	 * be invoked.
+	 */
+	private Hashtable<String, String> triggers;
+	/**
+	 * Takes w as a int parameter and assigns it to the instance variable,
+	 * weight.
+	 * 
+	 * @param w
+	 *            amount of space modified when taken into inventory.
+	 */
+	Item(int w) {
+		this.weight = w;
+	}
 
+	Item(String n) {
+		this.primaryName = n;
+	}
 
-    Item(Scanner s) throws NoItemException,
-        Dungeon.IllegalDungeonFormatException {
+	Item(Scanner s) throws NoItemException, Dungeon.IllegalDungeonFormatException {
 
-        messages = new Hashtable<String,String>();
-        triggers = new Hashtable<String,String>();
+		messages = new Hashtable<String, String>();
+		triggers = new Hashtable<String, String>();
 
-        // Read item name.
-        primaryName = s.nextLine();
-        if (primaryName.equals(Dungeon.TOP_LEVEL_DELIM)) {
-            throw new NoItemException();
-        }
+		// Read item name.
+		primaryName = s.nextLine();
+		if (primaryName.equals(Dungeon.TOP_LEVEL_DELIM)) {
+			throw new NoItemException();
+		}
 
-        // Read item weight.
-        weight = Integer.valueOf(s.nextLine());
+		// Read item weight.
+		weight = Integer.valueOf(s.nextLine());
 
-        // Read and parse verbs lines, as long as there are more.
-        String verbLine = s.nextLine();
-        while (!verbLine.equals(Dungeon.SECOND_LEVEL_DELIM)) {
-            if (verbLine.equals(Dungeon.TOP_LEVEL_DELIM)) {
-                throw new Dungeon.IllegalDungeonFormatException("No '" +
-                    Dungeon.SECOND_LEVEL_DELIM + "' after item.");
-            }
-            String[] verbParts = verbLine.split(":");
-            messages.put(verbParts[0],verbParts[1]);
-            /*
-             *  Check to see if the item specific verb has any events
-             *  which need to be triggered when acted upon. The '.bork'
-             *  file pattern identifies events by surrounding them with
-             *  brackets after the verb. 
-             *  
-             *  ie. verb[Event(param),Event(param)]:Message to display
-             *  
-             *  For triggers hashtable will store this information keyed
-             *  by the verb, the value being the event(s). If there are 
-             *  more than one events associated with a verb action, the
-             *  EventFactory will detect this and separate the events 
-             *  Accordingly.
-             */
-            for (String key : messages.keySet()) {
-            	if (key.contains("[")) {
-            		String[] eventParts = key.split("\\[");
-            		triggers.put(eventParts[0], eventParts[1].substring(0, eventParts[1].length()-1));
-            	}
-            }
-            
-            verbLine = s.nextLine();
-        }
-    }
+		// Read and parse verbs lines, as long as there are more.
+		String verbLine = s.nextLine();
+		while (!verbLine.equals(Dungeon.SECOND_LEVEL_DELIM)) {
+			if (verbLine.equals(Dungeon.TOP_LEVEL_DELIM)) {
+				throw new Dungeon.IllegalDungeonFormatException("No '" + Dungeon.SECOND_LEVEL_DELIM + "' after item.");
+			}
+			String[] verbParts = verbLine.split(":");
+			messages.put(verbParts[0], verbParts[1]);
+			/*
+			 * Check to see if the item specific verb has any events which need
+			 * to be triggered when acted upon. The '.bork' file pattern
+			 * identifies events by surrounding them with brackets after the
+			 * verb.
+			 * 
+			 * ie. verb[Event(param),Event(param)]:Message to display
+			 * 
+			 * For triggers hashtable will store this information keyed by the
+			 * verb, the value being the event(s). If there are more than one
+			 * events associated with a verb action, the EventFactory will
+			 * detect this and separate the events Accordingly.
+			 */
 
-    /**
-     * Used by parser to confirm whether user command input of an item name is valid 
-     * for an existing Item object.
-     * 
-     * @param name non-primary name string
-     * @return boolean confirming whether non-primary name is a valid name for Item
-     */
-    boolean goesBy(String name) {
-        // could have other aliases
-        return this.primaryName.equals(name);
-    }
+			for (String key : messages.keySet()) {
+				if (key.contains("[")) {
+					String[] eventParts = key.split(Pattern.quote("["));
+					triggers.put(eventParts[0], eventParts[1].substring(0, eventParts[1].length() - 1));
+				}
+			}
+			verbLine = s.nextLine();
+		}
+	}
 
-    String getPrimaryName() { return primaryName; }
+	/**
+	 * Used by parser to confirm whether user command input of an item name is
+	 * valid for an existing Item object.
+	 * 
+	 * @param name
+	 *            non-primary name string
+	 * @return boolean confirming whether non-primary name is a valid name for
+	 *         Item
+	 */
+	boolean goesBy(String name) {
+		// could have other aliases
+		return this.primaryName.equals(name);
+	}
 
-    public String getMessageForVerb(String verb) {
-        return messages.get(verb);
-    }
+	String getPrimaryName() {
+		return primaryName;
+	}
 
-    /**
-     * Simply returns Item object primary name
-     */
-    public String toString() {
-        return primaryName;
-    }
+	public String getMessageForVerb(String verb) {
+		return messages.get(verb);
+	}
+
+	/**
+	 * Simply returns Item object primary name
+	 */
+	public String toString() {
+		return primaryName;
+	}
 }
