64c2efda61b6810387e47b065fd5daafa70d92c9
Jacques Troussard <jtroussa@mail.umw.edu>
2016-11-17 14:30:56 -0500

    modified trigger method to match other events

--- src/ScoreEvent.java
+++ src/ScoreEvent.java
@@ -12,27 +12,28 @@ public class ScoreEvent extends Event {
 	 * Amount of points, parsed from event description.
 	 */
 	private int points;
-	
+
 	/**
 	 * Takes the points argument and sets the instance variable to it.
-	 *   
-	 * @param points amount of points as an integer to add/update player score with.
+	 * 
+	 * @param points
+	 *            amount of points as an integer to add/update player score
+	 *            with.
 	 */
 	public ScoreEvent(int points) {
 		this.points = points;
 	}
 
 	/**
-	 * This is a player status modifier trigger. The GameState playerScore variable is
-	 * updated by adding the points variable to the running total held by playerScore
-	 * (GameState)
+	 * This is a player status modifier trigger. The GameState playerScore
+	 * variable is updated by adding the points variable to the running total
+	 * held by playerScore (GameState)
 	 * 
 	 * @return message describing the points added and the current score
 	 */
 	@Override
-	String trigger() {
-		// TODO Auto-generated method stub
-		return null;
+	void trigger() {
+		// implement score incrementation.
 	}
 
 }
