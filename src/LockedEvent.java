677a9ddab92a18b4e6293f5b99e0192efca17297
Jacques Troussard <jtroussa@mail.umw.edu>
2016-11-17 14:43:20 -0500

    comments to michelle

--- src/LockedEvent.java
+++ src/LockedEvent.java
@@ -6,20 +6,23 @@
 
 /**
  * @author Michelle Booth
+ * @author Jacques Troussard
  *
  */
 public class LockedEvent extends Event {
-   
-   /**
-   * Locked status of exit
-   */
+
+	/**
+	 * Locked status of exit
+	 */
 
 	private boolean locked;
-	
+
 	/**
-	 * This is an exit manipulation where the locked status of the exit is changed
+	 * This is an exit manipulation where the locked status of the exit is
+	 * changed
 	 * 
-	 * @param locked boolean = the current locked state of the exit
+	 * @param locked
+	 *            boolean = the current locked state of the exit
 	 */
 	public LockedEvent(boolean locked) {
 		this.locked = locked;
@@ -27,8 +30,15 @@ public class LockedEvent extends Event {
 
 	/**
 	 * Activates the LockedEvent
-    * @param Item key name of the key object
+	 * 
+	 * @param Item
+	 *            key name of the key object
 	 * @return new locked status
+	 */
+
+	/*
+	 * Hi michelle! Your trigger shouldn't take in any arguments. Check out the
+	 * Command Factory Pattern, or other events to get a template.
 	 */
 	String trigger(key Item) {
 
