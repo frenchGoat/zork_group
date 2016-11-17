819224bb3a34fa1a692cf24e56aebd91729d4384
Jacques Troussard <jtroussa@mail.umw.edu>
2016-11-17 13:13:57 -0500

    finished if statement bank to find correct event sub class to trigger

--- src/EventFactory.java
+++ src/EventFactory.java
@@ -30,43 +30,38 @@ public class EventFactory {
 	 * another action/command taken in the game, ie. ItemSpecificCommand or some action
 	 * taken during trading/combat/dialog. Then it is parsed it into strings. These
      * Strings are being stored in a simple array. Event input will start with a specific
-     * event identifier followed by (if needed) arugments delimited with simple spaces
+     * event identifier followed by (if needed) arguments delimited with simple spaces
      * 
 	 * @param eventDescription string of eventDescription passed by initiating action
 	 * @return specific Event object as per the parsing
 	 */
 	public Event parse(String eventDescription) {
-		String eventBuffer[] = eventDescription.split(Pattern.quote(","));
-		int numOfEvents = eventBuffer.length;
-		
-		if (eventBuffer.length == 1) {
-			
-		}
-        String event = parts[0];
-        String param = parts.length >= 2 ? parts[1] : "";
-        if (verb.equalsIgnoreCase("die")) {
+		String[] parts = eventDescription.split(Pattern.quote("("));
+		String event = parts[0];
+		String param = parts.length >= 2 ? parts[1] : "";
+        if (event.equalsIgnoreCase("die")) {
             return new DieEvent(param);
         }
-        if (verb.equals("take")) {
-            return new TakeCommand(noun);
+        if (event.equals("Disappear")) {
+            return new DisappearEvent(param);
         }
-        if (verb.equals("drop")) {
-            return new DropCommand(noun);
+        if (event.equals("Lock")) {
+            return new LockedEvent(param);
         }
-        if (verb.equals("i") || verb.equals("inventory")) {
-            return new InventoryCommand();
+        if (event.equals("Score")) {
+            return new ScoreEvent(param);
         }
-        if (verb.equals("h") || verb.equals("health")) { 
-        	return new HealthCommand();
+        if (event.equals("Teleport")) {
+            return new TeleportEvent(param);
         }
-        if (MOVEMENT_COMMANDS.contains(verb)) {
-            return new MovementCommand(verb);
+        if (event.equals("Transform")) {
+            return new TransformEvent(param);
         }
-        if (parts.length == 2) {
-            return new ItemSpecificCommand(verb, noun);
+        if (event.equals("Wound")) {
+            return new WoundEvent(param);
+        } else {
+        	return new UnknownEvent(eventDescription);
         }
-        return new UnknownCommand(command);
-		return new UnknownEvent(eventDescription);
 	}
 
 }
