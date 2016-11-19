d12fcf983a819a8e20b8be0264c2deb496514cec
Jacques Troussard <jtroussa@mail.umw.edu>
2016-11-19 18:09:20 -0500

    merger server code

--- src/ScoreCommand.java
+++ src/ScoreCommand.java
@@ -27,6 +27,19 @@ public class ScoreCommand extends Command {
    * @return String The rank that corresponds to the score
    */
    String execute(){
+   String scoreMessage = null;
+   if(score >=25){
+      scoreMessage = "Beginning Trainer";
+   }
+   else if(score >=50 && score < 25){
+      scoreMessage = "Good Trainer";
+      }
+   else if(score >=75 && score < 50){
+      scoreMessage = "Expert Trainer";
+      }
+   else if(score <=100 && score > 75){
+      scoreMessage = "Master Trainer";
+      }
       return "score message";
    } 
 }
