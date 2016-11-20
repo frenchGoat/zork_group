b1d383da2afe510fd20a1827692eb34b9bde8228
kpayne4 <kpayne4@mail.umw.edu>
2016-11-20 16:15:33 -0500

    Update DieEvent.java

--- src/DieEvent.java
+++ src/DieEvent.java
@@ -1,32 +1,32 @@
+import java.io.*;
 /**
- * 
- */
-
-/**
- * @author Jacques Troussard
+ * @author Kaylee Payne
  *
  */
 public class DieEvent extends Event {
+    private String message = "You have died";
+    /**
+     * This is an end game trigger, in which the player has lost. Die events can be
+     * caused by reaching 0 in either player health or hunger status. This trigger 
+     * will reload the game from last restore point.
+     * 
+     */
+    public DieEvent() {
+        // TODO Auto-generated constructor stub
+    }
 
-	/**
-	 * This is an end game trigger, in which the player has lost. Die events can be
-	 * caused by reaching 0 in either player health or hunger status. This trigger 
-	 * will reload the game from last restore point.
-	 * 
-	 */
-	public DieEvent() {
-		// TODO Auto-generated constructor stub
-	}
-
-	/**
-	 * Activates the DieEvent.
-	 * 
-	 * @return message The end of game message
-	 */
-	@Override
-	String trigger() {
-		// TODO Auto-generated method stub
-		return message;
-	}
+    /**
+     * Activates the DieEvent.
+     * 
+     * @return message The end of game message
+     */
+    @Override
+    String trigger() {
+        GameState instance = GameState.instance();
+        try {
+            instance.restore(instance.DEFAULT_SAVE_FILE);
+        } catch (IOException | GameState.IllegalSaveFormatException | Dungeon.IllegalDungeonFormatException e) {}
+        return message;
+    }
 
 }
