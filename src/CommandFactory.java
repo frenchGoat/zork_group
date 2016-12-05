1737c70469166369b8b68e83fd664f5ddc907e82
Jacques Troussard <jtroussa@mail.umw.edu>
2016-12-04 19:48:02 -0500

    command parts parsing update
    user commands which are passed to CommandFactory with more than two
    words will now recognize every word after the second as an extension of
    the noun. The single letter or verb command funcutionality is retained
    by giving the noun variable the value of empty string if the command
    passed to the CommandFactory is only on part(string/word/character)
    long. Take command is retested on coin and normal objects.  Events
    tested too.

--- src/CommandFactory.java
+++ src/CommandFactory.java
@@ -3,78 +3,96 @@ import java.util.List;
 import java.util.Arrays;
 
 /**
- * The CommandFactory Class will take in user commands as strings, parse them and
- * try to determine what the player is trying to accomplish. If parsed commands
- * can be matched to valid sub-command classes, they will be created and executed,
- * possibly causing sub-event classes to be created, but always will return a string
- * to be printed on the console to alert the player as to the results of their 
- * input/actions.
+ * The CommandFactory Class will take in user commands as strings, parse them
+ * and try to determine what the player is trying to accomplish. If parsed
+ * commands can be matched to valid sub-command classes, they will be created
+ * and executed, possibly causing sub-event classes to be created, but always
+ * will return a string to be printed on the console to alert the player as to
+ * the results of their input/actions.
  * 
  * @author Jacques Troussard
  *
  */
 public class CommandFactory {
 
-    private static CommandFactory theInstance;
-    /**
-     * List of possible movement commands which the player may use.
-     */
-    public static List<String> MOVEMENT_COMMANDS = 
-        Arrays.asList("n","w","e","s","u","d" );
+	private static CommandFactory theInstance;
+	/**
+	 * List of possible movement commands which the player may use.
+	 */
+	public static List<String> MOVEMENT_COMMANDS = Arrays.asList("n", "w", "e", "s", "u",
+			"d");
 
-    public static synchronized CommandFactory instance() {
-        if (theInstance == null) {
-            theInstance = new CommandFactory();
-        }
-        return theInstance;
-    }
+	public static synchronized CommandFactory instance() {
+		if (theInstance == null) {
+			theInstance = new CommandFactory();
+		}
+		return theInstance;
+	}
 
-    private CommandFactory() {
-    }
+	private CommandFactory() {
+	}
 
-    /**
-     * This method receives the user command and attempts to parse it into strings. These
-     * Strings are being stored in a simple array. User input is assumed to come in two
-     * basic forms. 1) single character ex. 'q' 2) a multiple part command composed of 
-     * a verb key word and at least one noun.
-     * 
-     * @param command string of input received from player
-     * @return the specific command to execute
-     */
-    public Command parse(String command) {
-        String parts[] = command.split(" ");
-        String verb = parts[0];
-        String noun = parts.length >= 2 ? parts[1] : "";
-        if (verb.equals("save")) {
-            return new SaveCommand(noun);
-        }
-        if (verb.equals("help")) {
-            return new HelpCommand();
-        }
-        if (verb.equals("take")) {
-            return new TakeCommand(noun);
-        }
-        if (verb.equals("drop")) {
-            return new DropCommand(noun);
-        }
-        if (verb.equals("look")) {
-        	return new LookCommand();
-        }
-        if (verb.equals("score")) {
-        	return new ScoreCommand();
-        }
-        if (verb.equals("i") || verb.equals("inventory")) {
-            return new InventoryCommand();
-        }
-        if (verb.equals("h") || verb.equals("health")) { 
-        	return new HealthCommand();
-        }
-        if (MOVEMENT_COMMANDS.contains(verb)) {
-            return new MovementCommand(verb);
-        }
-        if (parts.length == 2) {
-            return new ItemSpecificCommand(verb, noun);
-        }
-        return new UnknownCommand(command);
-    }
+	/**
+	 * This method receives the user command and attempts to parse it into
+	 * strings. These Strings are being stored in a simple array. User input is
+	 * assumed to come in two basic forms. 1) single character ex. 'q' 2) a
+	 * multiple part command composed of a verb key word and at least one noun.
+	 * 
+	 * @param command
+	 *            string of input received from player
+	 * @return the specific command to execute
+	 */
+	public Command parse(String command) {
+		String parts[] = command.split(" ");
+		String verb = parts[0];
+		/*
+		 * Zork version three from professor assigned 'noun' variable this way;
+		 * String noun = parts.length >= 2 ? parts[1] : ""; taking anything
+		 * after the second word after the verb and throwing it away
+		 */
+		// In case the noun is multi worded, concats all the words into one
+		// String.
+		String noun = "";
+		if (parts.length >= 2) {
+			for (int i = 1; i < parts.length; i++) {
+				noun = noun.concat(parts[i] + " ");
+			}
+			noun = noun.substring(0, noun.length() - 1);
+		}else{
+			noun = "";
+		}
+		
+
+		if (verb.equals("save")) {
+			return new SaveCommand(noun);
+		}
+		if (verb.equals("help")) {
+			return new HelpCommand();
+		}
+		if (verb.equals("take")) {
+			return new TakeCommand(noun);
+		}
+		if (verb.equals("drop")) {
+			return new DropCommand(noun);
+		}
+		if (verb.equals("look")) {
+			return new LookCommand();
+		}
+		if (verb.equals("score")) {
+			return new ScoreCommand();
+		}
+		if (verb.equals("i") || verb.equals("inventory")) {
+			return new InventoryCommand();
+		}
+		if (verb.equals("h") || verb.equals("health")) {
+			return new HealthCommand();
+		}
+		if (MOVEMENT_COMMANDS.contains(verb)) {
+			return new MovementCommand(verb);
+		}
+		if (parts.length == 2) {
+			return new ItemSpecificCommand(verb, noun);
+		}
+		return new UnknownCommand(command);
+	}
 }
