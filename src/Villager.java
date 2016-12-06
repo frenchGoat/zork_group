123b43af63dee20a3587cc0d11fc08da1b31208d
Jacques Troussard <jtroussa@mail.umw.edu>
2016-12-06 01:36:20 -0500

    not much, place holder if anything

---
+++ src/Villager.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Villager extends NPC {
+
+	public Villager(String n) {
+		super(n);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Villager(Scanner scanner) {
+		super(scanner);
+		// TODO Auto-generated constructor stub
+	}
+
+}
