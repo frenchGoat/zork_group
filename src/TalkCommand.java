861919adebec3b95ae1316b5ac18aa422d55bf13
M Booth <M Booth>
2016-12-07 22:43:48 -0500

    Fixed talk to

--- src/TalkCommand.java
+++ src/TalkCommand.java
@@ -9,36 +9,42 @@ public class TalkCommand extends Command {
     /**
      * A string that is the name of the NPC the user wants to interact with.
      */
-    private String target;
+   private String target;
     
     /**
      * Creates a TalkCommand for the given NPC.
      * 
      * @param target The string name of an NPC.
      */
-    TalkCommand(String target) {
-       this.target = target.substring(3);
-    }
+   TalkCommand(String target) {
+      if(target.length()<=3){
+         this.target = null;
+      }
+      else{      
+         this.target = target.substring(3);
+      }
+   }
     
     /**
      * Will call on the target NPC interact() method to return the proper response.
      * 
      * @return answer The response taken from the target NPC conversation list in interact().
      */
-    public String execute() {
-    	if (target == null || target.trim().length() == 0) {
-            return "Talk to who?\n";
-        }
-    	Room currentRoom = 
+   public String execute() {
+      if (target == null || target.trim().length() == 0) {
+         return "Talk to who?\n";
+      }
+      Room currentRoom = 
                 GameState.instance().getAdventurersCurrentRoom();
-            try {
-				NPC theTarget = currentRoom.getNpcNamed(target);
-				theTarget.openDialogue();
-			} catch (NPC.NoNPCException e) {
-				// TODO Auto-generated catch block
-				return target + " isn't here to listen to your ramblings.\n";
-			}
+      try {
+         NPC theTarget = currentRoom.getNpcNamed(target);
+         theTarget.openDialogue();
+      } 
+      catch (NPC.NoNPCException e) {
+      		// TODO Auto-generated catch block
+         return target + " isn't here to listen to your ramblings.\n";
+      }
             
-            return Interpreter.cmdBrd + "\nOk now what?\n";
-    }
+      return Interpreter.cmdBrd + "\nOk now what?\n";
+   }
 }
